4cbac3aee92b3152d0bfe15c1fdeb7229e2ce796
Dan Andrus <danandrus@gmail.com>
2018-06-03 22:06:31 -0700

    Add documentation for Streamable

--- java/omnia/contract/Streamable.java
+++ java/omnia/contract/Streamable.java
@@ -2,7 +2,14 @@ package omnia.contract;
 
 import java.util.stream.Stream;
 
+/**
+ * A {@link Streamable} is an object whose contents can be streamed to and is compatible with
+ * Java's {@link Stream} facilities.
+ *
+ * @param <T> the type that can be streamed
+ */
 public interface Streamable<T> {
 
+  /** Creates and returns a stream of this object's contents. */
   Stream<T> stream();
 }
