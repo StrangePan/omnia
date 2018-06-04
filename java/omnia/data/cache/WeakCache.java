322252013ed39af042d9d8aa3180b6c125fd7232
Dan Andrus <danandrus@gmail.com>
2018-06-03 21:53:21 -0700

    Add documentation for WeakCache

--- java/omnia/data/cache/WeakCache.java
+++ java/omnia/data/cache/WeakCache.java
@@ -8,17 +8,35 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+/**
+ * A cache implementation that stores items using {@link WeakReferences} as a form of lazy,
+ * optional caching.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
 public class WeakCache<K, V> implements Container<K> {
   private final Map<K, WeakReference<V>> cache = new HashMap<>();
 
+  /** Creates a new, empty cache. */
   public WeakCache() {}
 
+  /** Checks if the cache contains the given key. */
   @Override
   public boolean contains(K key) {
     WeakReference<V> ref = cache.get(key);
     return ref != null && ref.get() != null;
   }
 
+  /**
+   * Retrieves and returns the cached value associated with {@code key} or, if necessary, resolves
+   * the given {@link Supplier} and stores its result in the cache.
+   *
+   * @param key the key to retrieve and, if unavailable, store the result of {@code factory} at
+   * @param factory the factory function to invoke if the desired cached value is missing
+   * @return the cached value if it exists or else the result of invoking {@link Supplier#get()} on
+   *     {@code factory}
+   */
   public V getOrCache(K key, Supplier<V> factory) {
     WeakReference<V> cachedReference = cache.get(key);
     V cachedResult = cachedReference != null ? cachedReference.get() : null;
@@ -29,6 +47,7 @@ public class WeakCache<K, V> implements Container<K> {
     return cachedResult;
   }
 
+  /** Retrieves the cached value associated with {@code key} if it exists. */
   public Optional<V> get(K key) {
     WeakReference<V> cachedReference = cache.get(key);
     V cachedResult = cachedReference != null ? cachedReference.get() : null;
