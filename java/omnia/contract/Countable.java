02f785dc5d315ba000bfaa6373b1db9c3dc17fc9
Dan Andrus <danandrus@gmail.com>
2018-06-03 22:19:16 -0700

    Add documentation for Countable

--- java/omnia/contract/Countable.java
+++ java/omnia/contract/Countable.java
@@ -1,6 +1,18 @@
 package omnia.contract;
 
+/**
+ * A {@link Countable} is an object whose contents can be counted, the sum of which can be
+ * represented by a non-negative integer anywhere between {@code 0} and {@link Integer#MAX_VALUE}.
+ *
+ * <p>Examples of things that are countable include common data structures, such as lists, sets, and
+ * maps.
+ */
 public interface Countable {
 
+  /**
+   * Get the sum of the items contained in this object.
+   *
+   * @return a non-negative integer between {@code 0} and {@link Integer#MAX_VALUE} inclusive.
+   */
   int count();
 }
