780bad79395e6a0ff918734a8d9787547460eaee
Dan Andrus <danandrus@gmail.com>
2018-06-03 22:21:48 -0700

    Add documentation for Pair

--- java/omnia/data/structure/Pair.java
+++ java/omnia/data/structure/Pair.java
@@ -2,9 +2,24 @@ package omnia.data.structure;
 
 import omnia.contract.Countable;
 
+/**
+ * A {@link Pair} is an set of exactly two objects, best used to represent items with little
+ * semantic correlation.
+ *
+ * @param <E1> the type of the first item
+ * @param <E2> the type of the second item
+ */
 public interface Pair<E1, E2> extends Countable {
 
+  /**
+   * Gets the first item contained in the pair. Subsequent calls to this method should always
+   * return the same value.
+   */
   E1 first();
 
+  /**
+   * Gets the second item contained in the pair. Subsequent calls to this method should always
+   * return the same value.
+   */
   E2 second();
 }
