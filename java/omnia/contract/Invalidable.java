f94e141de46570ce00b696235f5913cc92e135c6
Dan Andrus <danandrus@gmail.com>
2018-06-26 00:27:32 -0700

    Add Invalidable contract.

---
+++ java/omnia/contract/Invalidable.java
@@ -0,0 +1,20 @@
+package omnia.contract;
+
+/**
+ * Something that is {@link Invalidable} is one that exhibits observable behaviors that can be
+ * rendered invalidated or outdated by calling {@link #invalidate()}.
+ *
+ * <p>When an object is said to be valid, its private internal state or publicly observable state is
+ * in a consistent and accurate state, fit for dependents to safely use said state for all purposes.
+ * By contrast, an object that is said to be invalid is not fit for use by its dependents. Most
+ * objects that are {@link Invalidable} should provide an explicit method for making oneself valid
+ * again, although it is not an enforceable requirement.</p>
+ */
+public interface Invalidable {
+
+  /**
+   * Invalidates this object. This is a manual operation that should be called when a change in
+   * the world causes this object to no longer be safely in a valid state.
+   */
+  void invalidate();
+}
