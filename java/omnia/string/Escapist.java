8913995bdcf63d90154f8fd2ef37b06434b1210e
Dan Andrus <danandrus@gmail.com>
2018-10-01 21:31:25 -0700

    Add String escaping utility, Escapist

---
+++ java/omnia/string/Escapist.java
@@ -0,0 +1,59 @@
+package omnia.string;
+
+import java.util.Objects;
+import omnia.data.structure.Set;
+import omnia.data.structure.immutable.ImmutableSet;
+
+public final class Escapist {
+
+  private final char escapeChar;
+  private final Set<Character> specialChars;
+
+  private Escapist(char escapeChar) {
+    if (escapeChar == '\0') {
+      throw new IllegalArgumentException("Escape character cannot be \\0");
+    }
+    this.escapeChar = escapeChar;
+    specialChars = ImmutableSet.<Character>builder().add(escapeChar).build();
+  }
+
+  public String escape(String source) {
+    Objects.requireNonNull(source);
+    if (source.length() == 0) {
+      return source;
+    }
+
+    StringBuilder result = new StringBuilder();
+    int i = 0;
+    for (int j = 0; j < source.length(); j++) {
+      if (source.charAt(j) == escapeChar) {
+        result.append(source.subSequence(i, j));
+        i = j = j + 1;
+      }
+    }
+    if (i == source.length()) {
+      i = source.length() - 1;
+    }
+    if (i < 0) {
+      return "";
+    }
+    return result.append(source.subSequence(i, source.length())).toString();
+  }
+
+  public String unescape(String source) {
+    Objects.requireNonNull(source);
+    if (source.length() == 0) {
+      return source;
+    }
+
+    StringBuilder result = new StringBuilder();
+    int i = 0;
+    for (int j = 0; j < source.length(); j++) {
+      if (specialChars.contains(source.charAt(j))) {
+        result.append(source.subSequence(i, j - 1)).append(escapeChar);
+        i = j;
+      }
+    }
+    return result.append(source.subSequence(i, source.length())).toString();
+  }
+}
