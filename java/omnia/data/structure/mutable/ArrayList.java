d6e601b029300f6550bfca911547e8bebeb19de5
Daniel Andrus <danieleandrus@gmail.com>
2019-05-29 21:30:50 -0700

    Add static factory method for ArrayList

--- java/omnia/data/structure/mutable/ArrayList.java
+++ java/omnia/data/structure/mutable/ArrayList.java
@@ -1,8 +1,17 @@
 package omnia.data.structure.mutable;
 
+import java.util.Arrays;
 public final class ArrayList<E> extends MaskingList<E> {
 
   public ArrayList() {
-    super(new java.util.ArrayList<>());
+    this(new java.util.ArrayList<>());
+  }
+
+  private ArrayList(java.util.ArrayList<E> javaList) {
+    super(javaList);
+  }
+
+  public static <E> ArrayList<E> of(E[] items) {
+    return new ArrayList<>(new java.util.ArrayList<>(Arrays.asList(items)));
   }
 }
