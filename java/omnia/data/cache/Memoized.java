6a389695ca92951c9daa945b5ce50154698f2ba0
Daniel Andrus <danieleandrus@gmail.com>
2019-06-02 14:32:51 -0700

    Add factory methods to Memoized for the primitive memoizers

--- java/omnia/data/cache/Memoized.java
+++ java/omnia/data/cache/Memoized.java
@@ -1,5 +1,8 @@
 package omnia.data.cache;
 
+import java.util.function.DoubleSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.LongSupplier;
 import java.util.function.Supplier;
 import omnia.contract.Holder;
 
@@ -30,5 +33,38 @@ public interface Memoized<T> extends Holder<T> {
    */
   static <T> Memoized<T> memoize(Supplier<T> supplier) {
     return new SimpleMemoizer<>(supplier);
+  }
+
+  /**
+   * Creates a {@link MemoizedInt} implementation that uses the provided {@link IntSupplier} as the
+   * factory that supplies the value to be memoized.
+   *
+   * @param supplier the supplier that will create the value to be memoized
+   * @return a new {@link MemoizedInt} instance that will memoize the created value
+   */
+  static MemoizedInt memoizeInt(IntSupplier supplier) {
+    return new SimpleIntMemoizer(supplier);
+  }
+
+  /**
+   * Creates a {@link MemoizedLong} implementation that uses the provided {@link LongSupplier}
+   * as the factory that supplies the value to be memoized.
+   *
+   * @param supplier the supplier that will create the value to be memoized
+   * @return a new {@link MemoizedLong} instance that will memoize the created value
+   */
+  static MemoizedLong memoizeLong(LongSupplier supplier) {
+    return new SimpleLongMemoizer(supplier);
+  }
+
+  /**
+   * Creates a {@link MemoizedDouble} implementation that uses the provided {@link DoubleSupplier}
+   * as the factory that supplies the value to be memoized.
+   *
+   * @param supplier the supplier that will create the value to be memoized
+   * @return a new {@link MemoizedDouble} instance that will memoize the created value
+   */
+  static MemoizedDouble memoizeDouble(DoubleSupplier supplier) {
+    return new SimpleDoubleMemoizer(supplier);
   }
 }
