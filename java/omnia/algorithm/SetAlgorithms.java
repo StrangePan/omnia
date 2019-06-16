e728d0407bd2dde578efa1f4ac24814f1d552141
Daniel Andrus <danieleandrus@gmail.com>
2019-06-15 18:36:23 -0700

    Add SetAlgorithms

---
+++ java/omnia/algorithm/SetAlgorithms.java
@@ -0,0 +1,30 @@
+package omnia.algorithm;
+
+import static omnia.data.stream.Collectors.toSet;
+
+import omnia.data.structure.Set;
+import omnia.data.structure.immutable.ImmutableSet;
+
+public final class SetAlgorithms {
+  private SetAlgorithms() {}
+
+  public static <T> Set<T> unionOf(Set<? extends T> a, Set<? extends T> b) {
+    return ImmutableSet.<T>builder().addAll(a).addAll(b).build();
+  }
+
+  public static <T> Set<T> intersectionOf(Set<? extends T> a, Set<? extends T> b) {
+    Set<? extends T> min = (a.count() < b.count() ? a : b);
+    Set<? extends T> max = (min != a ? a : b);
+    return min.stream().filter(max::contains).collect(toSet());
+  }
+
+  public static <T> Set<T> differenceBetween(Set<? extends T> a, Set<? extends T> b) {
+    return a.stream().filter(item -> !b.contains(item)).collect(toSet());
+  }
+
+  public static boolean areDisjoint(Set<?> a, Set<?> b) {
+    Set<?> min = (a.count() < b.count() ? a : b);
+    Set<?> max = (min != a ? a : b);
+    return min.stream().anyMatch(max::contains);
+  }
+}
