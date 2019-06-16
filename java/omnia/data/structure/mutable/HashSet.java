7c5ac52d1569879f374f152c17bdc95cfdf96a25
Daniel Andrus <danieleandrus@gmail.com>
2019-06-15 18:32:04 -0700

    Add copy constructor to HashSet

--- java/omnia/data/structure/mutable/HashSet.java
+++ java/omnia/data/structure/mutable/HashSet.java
@@ -1,8 +1,18 @@
 package omnia.data.structure.mutable;
 
+import omnia.data.structure.Collection;
+
 public final class HashSet<E> extends MaskingSet<E> {
 
   public HashSet() {
     super(new java.util.HashSet<>());
+  }
+
+  private HashSet(Collection<? extends E> original) {
+    super(original.stream().collect(java.util.stream.Collectors.toSet()));
+  }
+
+  public static <E> HashSet<E> copyOf(Collection<? extends E> original) {
+    return new HashSet<>(original);
   }
 }
