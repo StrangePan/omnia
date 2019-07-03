054073f5005d207b6620ca6554b28c4f341d646d
Daniel Andrus <danieleandrus@gmail.com>
2019-07-02 21:35:14 -0700

    Add TypedMap, a heterogeneous map implementation

---
+++ java/omnia/data/structure/TypedMap.java
@@ -0,0 +1,72 @@
+package omnia.data.structure;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Optional;
+import omnia.data.structure.immutable.ImmutableMap;
+import omnia.data.structure.mutable.HashMap;
+import omnia.data.structure.mutable.MutableMap;
+
+public final class TypedMap {
+
+  public static final class Key<V> {
+
+    public static <V> Key<V> create() {
+      return new Key<>();
+    }
+  }
+
+  public static class Builder {
+    private final MutableMap<Key<?>, Object> map = new HashMap<>();
+
+    public <V> Builder putMapping(Key<V> key, V value) {
+      requireNonNull(key);
+      requireNonNull(value);
+      map.putMapping(key, value);
+      return this;
+    }
+
+    public Builder putMappings(TypedMap map) {
+      map.map.entries().forEach(entry -> this.map.putMapping(entry.key(), entry.value()));
+      return this;
+    }
+
+    public Builder remove(Key<?> key) {
+      requireNonNull(key);
+      map.removeKey(key);
+      return this;
+    }
+
+    public TypedMap build() {
+      return new TypedMap(map);
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static TypedMap of() {
+    return new TypedMap(ImmutableMap.of());
+  }
+
+  public static <V> TypedMap of(Key<V> key, V value) {
+    return new TypedMap(ImmutableMap.of(key, value));
+  }
+
+  private final Map<Key<?>, Object> map;
+
+  private TypedMap(Map<Key<?>, Object> map) {
+    this.map = ImmutableMap.copyOf(map);
+  }
+
+  public <V> Optional<V> get(Key<V> key) {
+    @SuppressWarnings("unchecked")
+    Optional<V> value = map.valueOf(key).map(v -> (V) v);
+    return value;
+  }
+
+  public Builder toBuilder() {
+    return new Builder().putMappings(this);
+  }
+}
