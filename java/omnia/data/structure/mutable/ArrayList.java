474c055a001da080130fffd747cde17b9b6037f8
Daniel Andrus <danieleandrus@gmail.com>
2019-11-21 23:02:16 -0800

    Add unit test for FilterIterator

--- java/omnia/data/structure/mutable/ArrayList.java
+++ java/omnia/data/structure/mutable/ArrayList.java
@@ -1,6 +1,7 @@
 package omnia.data.structure.mutable;
 
 import java.util.Arrays;
+import omnia.data.structure.List;
 
 public final class ArrayList<E> extends MaskingList<E> {
 
@@ -12,7 +13,14 @@ public final class ArrayList<E> extends MaskingList<E> {
     super(javaList);
   }
 
-  public static <E> ArrayList<E> of(E[] items) {
+  @SafeVarargs
+  public static <E> ArrayList<E> of(E...items) {
     return new ArrayList<>(new java.util.ArrayList<>(Arrays.asList(items)));
+  }
+
+  public static <E> ArrayList<E> copyOf(List<? extends E> otherList) {
+    ArrayList<E> newList = new ArrayList<>();
+    otherList.iterator().forEachRemaining(newList::add);
+    return newList;
   }
 }
