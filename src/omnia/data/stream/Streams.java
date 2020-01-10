b240f01bc59f8898b76115b353d17be4b414fe46
Dan Andrus <danieleandrus@gmail.com>
2020-01-10 08:38:11 -0800

    Streams.concat now accepts Stream<? extends T>

--- src/omnia/data/stream/Streams.java
+++ src/omnia/data/stream/Streams.java
@@ -6,14 +6,14 @@ public final class Streams {
   private Streams() {}
 
   @SafeVarargs
-  public static <T> Stream<T> concat(Stream<T>... streams) {
+  public static <T> Stream<T> concat(Stream<? extends T>... streams) {
     if (streams == null || streams.length == 0) {
       return Stream.empty();
     }
     if (streams.length == 1) {
-      return streams[0];
+      return streams[0].map(i -> i);
     }
-    Stream<T> concatenatedStream = streams[0];
+    Stream<T> concatenatedStream = streams[0].map(i -> i);
     for (int i = 1; i < streams.length; i++) {
       concatenatedStream = Stream.concat(concatenatedStream, streams[i]);
     }
