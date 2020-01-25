f36ab04f61ef43cb06506eff218cb91a52d0a932
Dan Andrus <danieleandrus@gmail.com>
2020-01-25 12:46:02 -0800

    Add static factory method to WritableObservableList

--- src/omnia/data/structure/rx/WritableObservableList.java
+++ src/omnia/data/structure/rx/WritableObservableList.java
@@ -4,4 +4,9 @@ import omnia.data.contract.Writable;
 import omnia.data.structure.mutable.MutableList;
 
 public interface WritableObservableList<E>
-    extends MutableList<E>, ObservableList<E>, Writable<ObservableList<E>> {}
+    extends MutableList<E>, ObservableList<E>, Writable<ObservableList<E>> {
+
+  static <E> WritableObservableList<E> create() {
+    return new WritableObservableListImpl<>();
+  }
+}
