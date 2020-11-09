997a83a6afceeb8cb33ef9d127236dc7ec3273ae
Dan Andrus <danieleandrus@gmail.com>
2020-11-09 02:24:37 -0800

    Add tests for ObservableState

---
+++ tests/omnia/data/structure/observable/ObservableStateTest.java
@@ -0,0 +1,53 @@
+package omnia.data.structure.observable;
+
+import io.reactivex.observers.TestObserver;
+import omnia.data.structure.tuple.Tuple;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class ObservableStateTest {
+
+  private final ObservableState<Integer> underTest = ObservableState.create(0);
+
+  @Test
+  public void observe_whenInit_emitsInitialValue() {
+    underTest.observe().test().assertValue(0);
+  }
+
+  @Test
+  public void observe_whenMutated_emitsMutatedValue() {
+    underTest.mutate(i -> i + 1).ignoreElement().blockingAwait();
+
+    underTest.observe().test().assertValue(1);
+  }
+
+  @Test
+  public void observe_thenMutate_emitsBothValues() {
+    TestObserver<Integer> observer = underTest.observe().test();
+
+    underTest.mutate(i -> i + 1).ignoreElement().blockingAwait();
+
+    observer.assertValues(0, 1);
+  }
+
+  @Test
+  public void mutate_returnsNewValue() {
+    underTest.mutate(i -> i + 1).test().assertValue(1);
+  }
+
+  @Test
+  public void mutateAndReturn_whenReturnsOtherValue_returnsOtherValue() {
+    underTest.mutateAndReturn(i -> Tuple.of(i + 1, 100)).test().assertValue(Tuple.of(1, 100));
+  }
+
+  @Test
+  public void observe_thenMutateAndReturn_returnsBothValues() {
+    TestObserver<Integer> observer = underTest.observe().test();
+
+    underTest.mutateAndReturn(i -> Tuple.of(i + 1, 100)).ignoreElement().blockingAwait();
+
+    observer.assertValues(0, 1);
+  }
+}
