cb7f62ec2b2bf78ce970160737019c6f9ea83427
Dan Andrus <danieleandrus@gmail.com>
2021-10-09 21:12:20 -0700

    Fix cache operator

--- src/omnia/reaktive/single/Cache.kt
+++ src/omnia/reaktive/single/Cache.kt
@@ -1,8 +1,9 @@
 package omnia.reaktive.single
 
+import com.badoo.reaktive.observable.autoConnect
 import com.badoo.reaktive.observable.firstOrError
 import com.badoo.reaktive.observable.replay
 import com.badoo.reaktive.single.Single
 import com.badoo.reaktive.single.asObservable
 
-fun <T> Single<T>.cache() = this.asObservable().replay(1).firstOrError()
+fun <T> Single<T>.cache() = this.asObservable().replay(1).autoConnect(1).firstOrError()
