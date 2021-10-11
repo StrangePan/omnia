89a3f966819d250c881067c50a42fe83dce0468e
Dan Andrus <danieleandrus@gmail.com>
2021-10-10 20:32:25 -0700

    Convert project to kotlin-native

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,34 @@
 plugins {
-  kotlin("jvm") version "1.5.31"
+  kotlin("multiplatform") version "1.5.31"
 }
 
 project.setBuildDir("out")
 
-dependencies {
-  api("com.badoo.reaktive:reaktive:1.2.0")
-  api("com.badoo.reaktive:reaktive-jvm:1.2.0")
-}
+kotlin {
+  jvm()
 
-sourceSets {
-  main {
-    java.srcDir("src")
-  }
-  test {
-    java.srcDir("tests")
-    dependencies {
-      implementation(kotlin("test"))
-      implementation(kotlin("test-junit"))
-      implementation("com.google.truth:truth:1.0")
-      implementation("org.mockito:mockito-core:3.2.0")
-      implementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
-      implementation("com.google.truth.extensions:truth-java8-extension:1.0")
-      implementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
-      implementation("org.jetbrains.kotlin:kotlin-test-junit:1.5.31")
-      implementation("com.badoo.reaktive:reaktive-testing:1.2.0")
+  sourceSets {
+    val jvmMain by getting {
+      kotlin.srcDir("src")
+      dependencies {
+        implementation(kotlin("test"))
+        api("com.badoo.reaktive:reaktive:1.2.0")
+        api("com.badoo.reaktive:reaktive-testing:1.2.0")
+        implementation("com.badoo.reaktive:reaktive-jvm:1.2.0")
+      }
+    }
+
+    val jvmTest by getting {
+      kotlin.srcDir("tests")
+      dependencies {
+        implementation(kotlin("test"))
+        implementation(kotlin("test-junit"))
+        implementation("com.google.truth:truth:1.0")
+        implementation("org.mockito:mockito-core:3.2.0")
+        implementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+        implementation("com.google.truth.extensions:truth-java8-extension:1.0")
+        implementation("com.badoo.reaktive:reaktive-testing:1.2.0")
+      }
     }
   }
 }
