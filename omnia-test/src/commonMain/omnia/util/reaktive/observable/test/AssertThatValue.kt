5b411767d901860292c37cbbbd82541cbfe475d4
Dan Andrus <danieleandrus@gmail.com>
2023-06-20 21:46:19 +0200

    Add more fluent Reaktive assertions

--- omnia-test/src/commonMain/omnia/util/reaktive/observable/test/AssertThatValue.kt
+++ omnia-test/src/commonMain/omnia/util/reaktive/observable/test/AssertThatValue.kt
@@ -1,6 +1,10 @@
 package omnia.util.reaktive.observable.test
 
+import com.badoo.reaktive.test.maybe.TestMaybeObserver
+import com.badoo.reaktive.test.maybe.assertSuccess
 import com.badoo.reaktive.test.observable.TestObservableObserver
+import com.badoo.reaktive.test.single.TestSingleObserver
+import com.badoo.reaktive.test.single.assertSuccess
 import omnia.util.test.assertGreater
 import omnia.util.test.assertGreaterOrEqual
 import omnia.util.test.fluent.Assertion
@@ -16,4 +20,20 @@ fun <T, R> TestObservableObserver<T>.assertThatValue(index: Int, mapper: (T) -> 
   assertGreaterOrEqual(0, index)
   assertGreater(index, this.values.size)
   return assertThat(mapper(this.values[index]))
+}
+
+fun <T> TestSingleObserver<T>.assertThatValue() =
+  this.assertThatValue { it }
+
+fun <T, R> TestSingleObserver<T>.assertThatValue(mapper: (T) -> R): Assertion<R> {
+  this.assertSuccess()
+  return assertThat(mapper(this.value))
+}
+
+fun <T> TestMaybeObserver<T>.assertThatValue() =
+  this.assertThatValue { it }
+
+fun <T, R> TestMaybeObserver<T>.assertThatValue(mapper: (T) -> R): Assertion<R> {
+  this.assertSuccess()
+  return assertThat(mapper(this.value))
 }
