429110ea61f78a14acbcc7142feeed1a31c94af7
Dan Andrus <danieleandrus@gmail.com>
2023-07-08 18:36:51 +0200

    Publish the MemoryBuffer completable

--- omnia/src/commonMain/omnia/io/MemoryBuffer.kt
+++ omnia/src/commonMain/omnia/io/MemoryBuffer.kt
@@ -4,15 +4,19 @@ import com.badoo.reaktive.completable.Completable
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.asCompletable
 import com.badoo.reaktive.observable.asObservable
-import com.badoo.reaktive.observable.doOnAfterNext
-import com.badoo.reaktive.observable.doOnAfterSubscribe
+import com.badoo.reaktive.observable.doOnBeforeNext
+import com.badoo.reaktive.observable.doOnBeforeSubscribe
+import com.badoo.reaktive.observable.publish
+import com.badoo.reaktive.observable.refCount
 
 class MemoryBuffer: Buffer {
   val lines = ArrayList<String>()
 
   override fun overwriteLinesWith(data: Observable<String>): Completable =
-    data.doOnAfterSubscribe { lines.clear() }
-      .doOnAfterNext { lines.add(it) }
+    data.doOnBeforeSubscribe { lines.clear() }
+      .doOnBeforeNext { lines.add(it) }
+      .publish()
+      .refCount()
       .asCompletable()
 
   override fun readLines(): Observable<String> = lines.asObservable()
