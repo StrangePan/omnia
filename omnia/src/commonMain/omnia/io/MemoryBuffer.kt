772a45ae6b988eb7abcca6521fcaffa50ee7a0f1
Dan Andrus <danieleandrus@gmail.com>
2023-07-09 15:16:10 +0200

    Add MemoryBuffer.copyOf and .toString

--- omnia/src/commonMain/omnia/io/MemoryBuffer.kt
+++ omnia/src/commonMain/omnia/io/MemoryBuffer.kt
@@ -8,9 +8,17 @@ import com.badoo.reaktive.observable.doOnBeforeNext
 import com.badoo.reaktive.observable.doOnBeforeSubscribe
 import com.badoo.reaktive.observable.publish
 import com.badoo.reaktive.observable.refCount
+import com.badoo.reaktive.single.Single
+import com.badoo.reaktive.single.map
+import omnia.data.structure.List
+import omnia.data.structure.mutable.ArrayList
+import omnia.util.reaktive.observable.collectIntoArrayList
 
-class MemoryBuffer: Buffer {
-  val lines = ArrayList<String>()
+class MemoryBuffer private constructor(val lines: ArrayList<String>): Buffer {
+
+  constructor() : this(ArrayList.create())
+
+  constructor(lines: List<String>) : this(ArrayList.copyOf(lines))
 
   override fun overwriteLinesWith(data: Observable<String>): Completable =
     data.doOnBeforeSubscribe { lines.clear() }
@@ -20,4 +28,14 @@ class MemoryBuffer: Buffer {
       .asCompletable()
 
   override fun readLines(): Observable<String> = lines.asObservable()
+
+  companion object {
+    fun copyOf(buffer: Buffer): Single<MemoryBuffer> {
+      return buffer.readLines().collectIntoArrayList().map(::MemoryBuffer)
+    }
+  }
+
+  override fun toString(): String {
+    return lines.joinToString(separator = "\n", postfix = "\n")
+  }
 }
