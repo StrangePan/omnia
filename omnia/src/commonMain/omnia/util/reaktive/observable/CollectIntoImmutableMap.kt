b210dd9e01edf81aeaea4eec87c4aed4e53788da
Dan Andrus <danieleandrus@gmail.com>
2023-07-09 15:14:39 +0200

    Clean up CollectIntoImmutableMap

--- omnia/src/commonMain/omnia/util/reaktive/observable/CollectIntoImmutableMap.kt
+++ omnia/src/commonMain/omnia/util/reaktive/observable/CollectIntoImmutableMap.kt
@@ -1,19 +1,19 @@
 package omnia.util.reaktive.observable
 
 import com.badoo.reaktive.observable.Observable
-import com.badoo.reaktive.single.map
+import com.badoo.reaktive.single.Single
 import omnia.data.structure.immutable.ImmutableMap
 import omnia.data.structure.tuple.Tuples.AtLeastCouple
 
-fun <K : Any, V: Any> Observable<AtLeastCouple<out K, out V>>.collectIntoImmutableMap() =
+fun <K : Any, V: Any> Observable<AtLeastCouple<out K, out V>>.collectIntoImmutableMap(): Single<ImmutableMap<K, V>> =
   this.collectInto(
-    { ImmutableMap.builder<K, V>() },
-    { builder, couple -> builder.putMapping(couple.first, couple.second) })
-    .map(ImmutableMap.Builder<K, V>::build)
+    ImmutableMap.Companion::builder,
+    { builder, couple -> builder.putMapping(couple.first, couple.second) },
+    ImmutableMap.Builder<K, V>::build)
 
-fun <T : Any, K : Any, V: Any> Observable<T>.collectIntoImmutableMap(
-  keyMapper: (T) -> K, valueMapper: (T) -> V) =
+fun <T : Any, K : Any, V: Any> Observable<T>.collectIntoImmutableMap(keyMapper: (T) -> K, valueMapper: (T) -> V):
+  Single<ImmutableMap<K, V>> =
   this.collectInto(
-    { ImmutableMap.builder<K, V>() },
-    { builder, item -> builder.putMapping(keyMapper(item), valueMapper(item)) })
-    .map(ImmutableMap.Builder<K, V>::build)
+    ImmutableMap.Companion::builder,
+    { builder, item -> builder.putMapping(keyMapper(item), valueMapper(item)) },
+    ImmutableMap.Builder<K, V>::build)
