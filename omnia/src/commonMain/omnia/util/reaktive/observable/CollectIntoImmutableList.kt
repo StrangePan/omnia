b9652144dccfce9977efe7cbcdc5556d1ecf912d
Dan Andrus <danieleandrus@gmail.com>
2023-07-09 15:14:33 +0200

    Clean up CollectIntoImmutableList

--- omnia/src/commonMain/omnia/util/reaktive/observable/CollectIntoImmutableList.kt
+++ omnia/src/commonMain/omnia/util/reaktive/observable/CollectIntoImmutableList.kt
@@ -1,9 +1,8 @@
 package omnia.util.reaktive.observable
 
 import com.badoo.reaktive.observable.Observable
-import com.badoo.reaktive.single.map
+import com.badoo.reaktive.single.Single
 import omnia.data.structure.immutable.ImmutableList
 
-fun <T : Any> Observable<T>.collectIntoImmutableList() =
-  this.collectInto(ImmutableList.Companion::builder, ImmutableList.Builder<T>::add)
-    .map(ImmutableList.Builder<T>::build)
+fun <T : Any> Observable<T>.collectIntoImmutableList(): Single<ImmutableList<T>> =
+  this.collectInto(ImmutableList.Companion::builder, ImmutableList.Builder<T>::add, ImmutableList.Builder<T>::build)
