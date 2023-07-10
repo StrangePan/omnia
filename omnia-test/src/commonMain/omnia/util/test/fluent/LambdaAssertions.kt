26dcd2d4fcdbd4f9dbb0a2e5835a1c17fac889c7
Dan Andrus <danieleandrus@gmail.com>
2023-07-10 20:26:38 +0200

    Add assertThatCode

--- omnia-test/src/commonMain/omnia/util/test/fluent/LambdaAssertions.kt
+++ omnia-test/src/commonMain/omnia/util/test/fluent/LambdaAssertions.kt
@@ -4,5 +4,9 @@ import kotlin.reflect.KClass
 import kotlin.test.assertFailsWith
 import omnia.util.test.fluent.Assertion.Companion.assertThat
 
-fun <E : Exception> Assertion<()->Unit>.failsWith(exceptionClass: KClass<E>): Assertion<E> =
-  assertThat(assertFailsWith(exceptionClass, this.message, this.actual))
+fun <E : Throwable> Assertion<()->Unit>.failsWith(throwableClass: KClass<E>): Assertion<E> =
+  assertThat(assertFailsWith(throwableClass, this.message, this.actual))
+
+fun Assertion<()->Unit>.fails(): Assertion<Throwable> = this.failsWith(Throwable::class)
+
+fun assertThatCode(code: ()->Unit) = assertThat(code)
