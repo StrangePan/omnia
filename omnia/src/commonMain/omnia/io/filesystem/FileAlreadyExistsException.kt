e7a942f90ca7423011b3807ffb191399b6aa4b59
Dan Andrus <danieleandrus@gmail.com>
2023-11-22 20:56:03 +0100

    Make file path accessible in FileAlreadyExistsException

--- omnia/src/commonMain/omnia/io/filesystem/FileAlreadyExistsException.kt
+++ omnia/src/commonMain/omnia/io/filesystem/FileAlreadyExistsException.kt
@@ -4,15 +4,27 @@ import omnia.io.IOException
 
 class FileAlreadyExistsException: IOException {
 
-  constructor(file: FileSystemObject): super(file.fullPath.toString())
+  val path: AbsolutePath
 
-  constructor(file: FileSystemObject, message: String): super(msg(file, message))
+  constructor(file: FileSystemObject): super(file.fullPath.toString()) {
+    this.path = file.fullPath
+  }
 
-  constructor(file: FileSystemObject, message: String, cause: Throwable): super(msg(file, message), cause)
+  constructor(file: FileSystemObject, message: String): super(msg(file, message)) {
+    this.path = file.fullPath
+  }
 
-  constructor(file: FileSystemObject, cause: Throwable): super(file.fullPath.toString(), cause)
+  constructor(file: FileSystemObject, message: String, cause: Throwable): super(msg(file, message), cause) {
+    this.path = file.fullPath
+  }
 
-  constructor(path: AbsolutePath, cause: Throwable): super(path.toString(), cause)
+  constructor(file: FileSystemObject, cause: Throwable): super(file.fullPath.toString(), cause) {
+    this.path = file.fullPath
+  }
+
+  constructor(path: AbsolutePath, cause: Throwable): super(path.toString(), cause) {
+    this.path = path
+  }
 
   companion object {
     fun msg(file: FileSystemObject, message: String) =
